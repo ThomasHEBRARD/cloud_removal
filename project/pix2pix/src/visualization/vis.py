2b89adbbff2953cb202ab98c0de39e9c426d3c82
thebrard <43602282+ThomasHEBRARD@users.noreply.github.com>
2023-06-08 11:05:46 +0200

    better plotting

--- project/pix2pix/src/visualization/vis.py
+++ project/pix2pix/src/visualization/vis.py
@@ -52,14 +52,67 @@ for i in range(2, len(["B04", "B03", "B02", "B08"]) + 2):
         "image": (((input_pred[:, :, :, i] + 1) / 2) * 255).astype(np.uint8),
     }
 
+
+########################################################
+########                  PLOT                  ########
+########################################################
 rows = BATCH_SIZE
 cols = 2 + len(models_bands)  # For cloudy_input, ground_truth and each model output
-fig, axes = plt.subplots(rows, cols, figsize=(16 * cols, 8 * rows))  # Adjust the figure size accordingly
+# fig, axes = plt.subplots(rows, cols, figsize=(16 * cols, 8 * rows))  # Adjust the figure size accordingly
+
+# for image_idx in range(BATCH_SIZE):
+
+#     # Create the figure and axes array
+
+#     cloudy_input = np.stack(
+#         (
+#             input_dict[f"s2_B04"]["image"][image_idx],
+#             input_dict[f"s2_B03"]["image"][image_idx],
+#             input_dict[f"s2_B02"]["image"][image_idx],
+#         ),
+#         axis=-1,
+#     )
+#     # Display cloudy_input
+#     axes[image_idx, 0].imshow(cloudy_input)
+#     axes[image_idx, 0].set_title(f"Sentinel-2 Cloudy Input")
+#     axes[image_idx, 0].axis("off")
+
+#     # Display ground_truth
+#     axes[image_idx, 1].imshow(ground_truth[image_idx])
+#     axes[image_idx, 1].set_title(f"Ground Truth")
+#     axes[image_idx, 1].axis("off")
+
+#     # Display model outputs
+
+#     for j, (model, bands, filename) in enumerate(models_bands):
+#         print(f"MODEL: {model}", len(bands)+2, input_pred.shape)
+#         model = load_model(model)
+#         input_to_show = input_pred[:,:,:,:len(bands)+2]
+
+#         output = model.predict(input_to_show)
+#         generated_image = ((output + 1) / 2 * 255).astype(np.uint8)
+
+#         axes[image_idx, j + 2].imshow(generated_image[image_idx])
+#         axes[image_idx, j + 2].set_title(f"Model {j+1} Output")
+#         axes[image_idx, j + 2].axis("off")
+
+# plt.tight_layout()
+# fig.savefig(f"vis/visualisation.png")  # Save the composite figure
+# plt.close()
+
+from PIL import Image
+
+# Define the image dimensions and separator size
+image_width, image_height = 256, 256  # Replace with your actual image size
+separator_size = 10  # Size of white separator between images in pixels
+
+# Lists to store PIL Image objects
+all_images = []
 
 for image_idx in range(BATCH_SIZE):
+    row_images = []
 
-    # Create the figure and axes array
-
+    # Create the cloudy input
     cloudy_input = np.stack(
         (
             input_dict[f"s2_B04"]["image"][image_idx],
@@ -68,18 +121,12 @@ for image_idx in range(BATCH_SIZE):
         ),
         axis=-1,
     )
-    # Display cloudy_input
-    axes[image_idx, 0].imshow(cloudy_input)
-    axes[image_idx, 0].set_title(f"Sentinel-2 Cloudy Input")
-    axes[image_idx, 0].axis("off")
+    row_images.append(Image.fromarray((cloudy_input).astype(np.uint8)))
 
-    # Display ground_truth
-    axes[image_idx, 1].imshow(ground_truth[image_idx])
-    axes[image_idx, 1].set_title(f"Ground Truth")
-    axes[image_idx, 1].axis("off")
+    # Add the ground truth
+    row_images.append(Image.fromarray((ground_truth[image_idx]).astype(np.uint8)))
 
-    # Display model outputs
-
+    # Apply each model and add its output
     for j, (model, bands, filename) in enumerate(models_bands):
         print(f"MODEL: {model}", len(bands)+2, input_pred.shape)
         model = load_model(model)
@@ -88,10 +135,20 @@ for image_idx in range(BATCH_SIZE):
         output = model.predict(input_to_show)
         generated_image = ((output + 1) / 2 * 255).astype(np.uint8)
 
-        axes[image_idx, j + 2].imshow(generated_image[image_idx])
-        axes[image_idx, j + 2].set_title(f"Model {j+1} Output")
-        axes[image_idx, j + 2].axis("off")
+        row_images.append(Image.fromarray(generated_image[image_idx]))
 
-plt.tight_layout()
-fig.savefig(f"vis/visualisation.png")  # Save the composite figure
-plt.close()
+    # Stitch together the images for this row
+    stitched_row_image = Image.new('RGB', ((image_width + separator_size) * cols - separator_size, image_height), "white")
+    for idx, img in enumerate(row_images):
+        stitched_row_image.paste(img, ((image_width + separator_size) * idx, 0))
+
+    all_images.append(stitched_row_image)
+
+# Stitch together all rows into one final image
+final_image = Image.new('RGB', ((image_width + separator_size) * cols - separator_size, (image_height + separator_size) * rows - separator_size), "white")
+
+for idx, img in enumerate(all_images):
+    final_image.paste(img, (0, (image_height + separator_size) * idx))
+
+# Save the final stitched image
+final_image.save("vis/final_image.png")
